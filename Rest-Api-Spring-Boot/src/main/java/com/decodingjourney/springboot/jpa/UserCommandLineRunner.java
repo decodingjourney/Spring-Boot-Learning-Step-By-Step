d2bc36df56fed9a52d5a4a77f7be8131ae034c99
anandkumarjhaic <37099338+anandkumarjhaic@users.noreply.github.com>
2018-11-02 20:58:10 +0530

    Integration testing started

--- Rest-Api-Spring-Boot/src/main/java/com/decodingjourney/springboot/jpa/UserCommandLineRunner.java
+++ Rest-Api-Spring-Boot/src/main/java/com/decodingjourney/springboot/jpa/UserCommandLineRunner.java
@@ -4,7 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
+@Component
 public class UserCommandLineRunner implements CommandLineRunner{
 	
 	private static final Logger log = LoggerFactory
@@ -21,9 +23,26 @@ public class UserCommandLineRunner implements CommandLineRunner{
 		repository.save(new User("Munna", "User"));
 		repository.save(new User("Sonam", "Admin"));
 		
-		for(User user: repository.findAll()) {
-			log.info(user.toString());
-		}
+		log.info("-------------------------------");
+        log.info("Finding all users");
+        log.info("-------------------------------");
+        for (User user : repository.findAll()) {
+            log.info(user.toString());
+        }
+        /*log.info("-------------------------------");
+        log.info("deleting all users");
+        log.info("-------------------------------");
+        repository.deleteAll();
+        for (User user : repository.findAll()) {
+            log.info(user.toString());
+        }*/
+        
+        log.info("-------------------------------");
+        log.info("Finding Admin users");
+        log.info("-------------------------------");
+        for (User user : repository.findByRole("Admin")) {
+            log.info(user.toString());
+        }
 	}
 	
 	
--- Rest-Api-Spring-Boot/src/main/java/com/decodingjourney/springboot/jpa/UserRepository.java
+++ Rest-Api-Spring-Boot/src/main/java/com/decodingjourney/springboot/jpa/UserRepository.java
@@ -1,7 +1,10 @@
 package com.decodingjourney.springboot.jpa;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Long> {
+	List<User> findByRole(String role);
 
 }
